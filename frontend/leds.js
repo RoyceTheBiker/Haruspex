059ebed75234184fd0b817a3c8a4b5485a852796
Royce Souther <osgnuru@gmail.com>
2024-12-03 13:53:55 -0700

    👌 IMPROVE: Loading BS icons

--- frontend/leds.js
+++ frontend/leds.js
@@ -1,3 +1,15 @@
+
+let ledState = {
+  0: 'off',
+  1: 'off',
+  2: 'off',
+  3: 'off',
+  4: 'off',
+  5: 'off',
+  6: 'off',
+  7: 'off'
+};
+
 function leds() {
   var jqScript = document.createElement('script');
   jqScript.type = 'text/javascript';
@@ -16,29 +28,41 @@ function leds() {
     var bsScript = document.createElement('script');
     bsScript.type = 'text/javascript';
     bsScript.src = 'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js';
-    bsScript.addEventListener('load', () => {
-      let ledState = {
-        0: 'off',
-        1: 'off',
-        2: 'off',
-        3: 'off',
-        4: 'off',
-        5: 'off',
-        6: 'off',
-        7: 'off'
-      };
+    document.head.appendChild(bsScript);
+    var bsTheme = document.createElement('link');
+    bsTheme.type = 'text/css';
+    bsTheme.rel = 'stylesheet';
+    bsTheme.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css';
+    document.head.appendChild(bsTheme);
+    var bsTheme = document.createElement('link');
+    bsTheme.type = 'font/woff';
+    bsTheme.rel = 'stylesheet';
+    bsTheme.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/fonts/bootstrap-icons.woff';
+    document.head.appendChild(bsTheme);
+    var bsTheme = document.createElement('link');
+    bsTheme.type = 'font/woff';
+    bsTheme.rel = 'stylesheet';
+    bsTheme.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/fonts/bootstrap-icons.woff2';
+    document.head.appendChild(bsTheme);
+
+    bsScript.addEventListener('load', () => {      
       $.get('/api/ledsalloff');
       for(let i = 0; i < 8; i++) {
-        $('#mainDiv').append('<p>PIN ' + i + ' is ' + ledState[i] + ' </p>');
-        $('#mainDiv').append('<p><button class="btn btn-info">Turn ' + ledState[i] === 'off' ? 'ON' : 'OFF' + '</button></p>');
+        $('#mainDiv').append('<p id="pinState' + i + '">PIN ' + i + ' is ' + ledState[i] + ' </p>');
+        $('#mainDiv').append('<p><i class="bi bi-lightbulb"></i><button id="button_' + i + '" class="btn btn-info">Turn</button></p>');
+        let newText = 'Turn ' + (ledState[i] === 'off' ? 'ON' : 'OFF');
+        $('#button_' + i).html(newText);
+        $('#button_' + i).click( () => buttonClick(i));
       }
     });
-    document.head.appendChild(bsScript);
   });
   document.head.appendChild(jqScript);
 }
 
 function buttonClick(buttonIndex) {
-  $.get('/api/led' + buttonIndex + '/' + ledState[i] === 'off' ? 'on' : 'off');
-  ledState[i] = ledState[i] === 'off' ? 'on' : 'off';
+  console.log('button click');
+  $.get('/api/led/' + (ledState[buttonIndex] === 'off' ? 'on' : 'off') + '/' + buttonIndex);
+  ledState[buttonIndex] = ledState[buttonIndex] === 'off' ? 'on' : 'off';
+  $('#pinState' + buttonIndex).html('PIN ' + buttonIndex + ' is ' + (ledState[buttonIndex] === 'off' ? 'off' : 'on'));
+  $('#button_' + buttonIndex).html('Turn ' + (ledState[buttonIndex] === 'off' ? 'ON' : 'OFF'));
 }
